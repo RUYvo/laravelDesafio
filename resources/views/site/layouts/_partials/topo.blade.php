634df7c92e45063ab43cf6058917b616013201cd
RUYvo <126499431+RUYvo@users.noreply.github.com>
2024-05-14 21:37:38 -0300

    Update topo.blade.php

--- resources/views/site/layouts/_partials/topo.blade.php
+++ resources/views/site/layouts/_partials/topo.blade.php
@@ -11,7 +11,7 @@
             <li><a href="{{ route('app.clientes') }}">Clientes</a></li>
             <li><a href="{{ route('app.fornecedores') }}">Fornecedores</a></li>
             <li><a href="{{ route('app.produtos') }}">Produtos</a></li>
-            <li><a href="{{ route('login') }}">Login</a></li>
+            <li><a href="{{ route('site.login') }}">Login</a></li>
         </ul>
     </div>
 </div>
